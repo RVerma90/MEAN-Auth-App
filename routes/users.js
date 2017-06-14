438a191180bd621798aea30262b3be7b5e1a34d8
RVerma90 <irahulverma90@gmail.com>
2017-06-14 01:17:45 +0100

    API Authentication and Token

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 const User = require ('../models/user');
 
@@ -27,11 +28,41 @@ router.post('/register', (req, res, next) => {
 });
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-  res.send('AUTHENTICATE');
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user){
+      return res.json({success: false, msg: 'User not found'});
+    }
+    // Compares user entered pw with jwt password
+    User.comparePassword(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch){
+          const token = jwt.sign(user, config.secret, {
+            expiresIn: 604800 // 1 week in seconds
+          });
+
+          res.json({
+            success: true,
+            token: 'JWT ' + token,
+            user: {
+              id: user._id,
+              name: user.name,
+              username: user.username,
+              email: user.email
+            }
+          });
+        } else {
+          return res.json({success: false, msg: 'Wrong password'});
+        }
+    });
+  });
 });
 // Profile
-router.get('/profile', (req, res, next) => {
-  res.send('PROFILE');
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+  res.json({user: req.user});
 });
 
 module.exports = router;
