11c202158af3794c329701faa22d97f57f113d8f
RVerma90 <irahulverma90@gmail.com>
2017-06-14 11:57:45 +0100

    Added Angular 4 and Material Design

---
+++ angular2-src/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// FormsModule required, and must be added to imports for two-way data binding
+import { FormsModule } from '@angular/forms';
+// HttpModule required, and must be added to imports for Http request
+import { HttpModule } from '@angular/http';
+// MaterialModule from Material design
+import { MaterialModule } from "@angular/material";
+// BrowserAnimationsModule for animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+// RouterModule, Routes for route setup
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
